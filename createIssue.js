e228d94055ed87524d86f8f16345c9cd7c8eb4a1
Sanjeev Pande <sanjeev.pande@getzephyr.com>
2016-11-09 17:26:54 +0000

    Update create issue form

--- createIssue.js
+++ createIssue.js
@@ -1,3 +1,14 @@
+// to check Cannot read property 'msie' of undefined
+jQuery.browser = {};
+(function() {
+    jQuery.browser.msie = false;
+    jQuery.browser.version = 0;
+    if (navigator.userAgent.match(/MSIE ([0-9]+)\./)) {
+        jQuery.browser.msie = true;
+        jQuery.browser.version = RegExp.$1;
+    }
+})();
+
 $.ajax({
 	url: '/quickCreateIssue',
 	method: 'GET',
@@ -19,4 +30,24 @@ function quickCreateIssue(resposne) {
 		$issueContainer.append($div);
 	});
 	$('#issueWrapper').html($issueContainer);
+
+	manageEvents();
+}
+
+function manageEvents() {
+	//datePicker
+	AJS.$('#duedate').datePicker({'overrideBrowserDefault': true});
+	AJS.$('#duedate-trigger').on('click', function() {
+		AJS.$('#duedate').trigger('click');
+	});
+
+	//single select
+	AJS.$('.single-user-picker').auiSelect2();
+
+	//multiselect
+	AJS.$('#components').auiSelect2();
+	AJS.$('#versions').auiSelect2();
+	AJS.$('#fixVersions').auiSelect2();
+
+	AJS.$('.ajs-multi-select-placeholder').hide();
 }
